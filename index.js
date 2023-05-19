c66ab3460ae4bb4e1e3c31109da508d5f0b1bf35
biroue isaac <biroueisaac@gmail.com>
2023-05-19 17:42:00 +0100

    modify the getAll route handler

--- index.js
+++ index.js
@@ -12,24 +12,30 @@ app.use(cors())
 //fetching all contacts to our phonebook via a get request
 app.get('/api/contacts', (request, response) => {
     Person.find({}).then(persons => {
-        if (persons) {
-            response.json(persons)
-        }
-        else {
-            response.status(404).end()
-        }
+        response.json(persons)
     })
-        .catch(error => {
-            console.log(error)
-            response.status(500).end()
-        })
 
 })
 //fetching single contact info to our phonebook via get request
 app.get('/api/contacts/:id', (request, response) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person)
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
     })
+        .catch(error => {
+            console.log(error)
+            response.status(400).send({ error: 'malformatted id' })
+        })
+})
+//deleting contact to our phonebook via delete request
+
+app.delete('/api/contacts/:id', (request, response) => {
+    const id = Number(request.params.id)
+    contacts = contacts.filter(contact => contact.id !== id)
+    response.status(204).end()
 })
 //sending data to our phonebook via post request
 app.post('/api/contacts/', (request, response) => {
